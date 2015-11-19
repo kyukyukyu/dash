d7fe3d24259447082728581e72b0f99612573148
Sanggyu Nam <pokeplus@gmail.com>
2015-11-19 13:58:03 +0900

    Add a new command update_catalog to manage.py
    * Closes #9.

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import importlib
 import os
 import sys
 import subprocess
@@ -7,6 +8,7 @@ from flask.ext.script import Manager, Shell, Server
 from flask.ext.migrate import MigrateCommand
 
 from dash.app import create_app
+from dash.catalog.models import Campus
 from dash.user.models import User
 from dash.settings import DevConfig, ProdConfig
 from dash.database import db
@@ -33,6 +35,14 @@ def test():
     import pytest
     exit_code = pytest.main(['tests', '--verbose'])
     return exit_code
+
+
+@manager.option('-c', '--code', dest='campus_code')
+def update_catalog(campus_code):
+    """Updates catalog of a campus using user-written scraper."""
+    campus = Campus.query.filter_by(code=campus_code)
+    scraper = importlib.import_module(campus.scraper)
+    scraper.update()
 
 manager.add_command('server', Server())
 manager.add_command('shell', Shell(make_context=_make_context))
