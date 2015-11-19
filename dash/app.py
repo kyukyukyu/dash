adb653f9c8d855f2a04b7861a0df95fbb58bcf91
Sanggyu Nam <pokeplus@gmail.com>
2015-11-20 03:50:28 +0900

    Fix wrong import statement for log handler

--- dash/app.py
+++ dash/app.py
@@ -28,8 +28,7 @@ def create_app(config_object=ProdConfig):
     register_errorhandlers(app)
     if not app.debug:
         import logging
-        from logging.handlers import StreamHandler
-        log_handler = StreamHandler()
+        log_handler = logging.StreamHandler()
         log_handler.setLevel(logging.ERROR)
         app.logger.addHandler(log_handler)
     return app
